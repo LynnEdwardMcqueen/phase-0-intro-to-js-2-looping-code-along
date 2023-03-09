8bf6a56cf8df063d7890fcb118fb8e2cc418d742
Lynn Edward McQueen <lynn.edward.mcqueen@gmail.com>
2023-03-09 12:28:47 -0700

    Adds looping routines

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+
+function writeCards(nameArray, gift) {
+    let thankYous = [];
+
+    for (let i = 0; i < nameArray.length; i++) {
+        thankYous.push(`Thank you, ${nameArray[i]}, for the wonderful ${gift} gift!`)
+    }
+    // debugger
+    return(thankYous);
+}
+
+function countDown(number) {
+    for (let i = number; i >= 0; i--) {
+        console.log(i);
+    }
+}
+
